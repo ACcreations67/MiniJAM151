7b6b4d27316f76b9e6a20911f0b6093010098371
17thdimensionstudio <112668279+ACcreations67@users.noreply.github.com>
2024-02-13 22:17:27 -0800

    updated shaders (v0.2.2)

--- MiniJAM151/Assets/GameManagerScript.cs
+++ MiniJAM151/Assets/GameManagerScript.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    [SerializeField] int BUYINVALUE;
-    public List<string> cardConversionTable = new() { "AS", "2S", "3S", "4S", "5S", "6S", "AS", "8S", "9S", "10S", "JS", "QS", "KS", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"};
+    public static List<string> cardConversionTable = new() { "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "2D", "3D", "4D", "5D", "6D", "8D", "9D", "10D", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "AC", "AD", "AH" };
+
 
     public List<int> playerCards;
-    public int playerChips = 20;
-    public int playerBet = 0;
-    public bool playerFolded = false;
+    public List<int> playerPowerups;
+    public int playerActiveCard;
+    public bool playerShreded = false;
+    public bool playerAtWar = false;
+
 
     public List<int> bot1Cards;
-    public int bot1Chips = 20;
-    public int bot1Bet;
-    public List<int> bot2Cards;
-    public int bot2Chips = 20;
-    public int bot2Bet;
-    public List<int> bot3Cards;
-    public int bot3Chips = 20;
-    public List<int> bot4Cards;
-    public int bot4Chips = 20;
+    public List<int> bot1Powerups;
+    public int bot1ActiveCard;
+    public bool bot1Shreded = false;
+    bool bot1AtWar = false;
 
+    public List<int> bot2Cards;
+    public List<int> bot2Powerups;
+    public int bot2ActiveCard;
+    public bool bot2Shreded = false;
+    bool bot2AtWar = false;
+
+    public List<int> bot3Cards;
+    public List<int> bot3Powerups;
+    public int bot3ActiveCard;
+    public bool bot3Shreded = false;
+    bool bot3AtWar = false;
+
+    public List<int> bot4Cards;
+    public List<int> bot4Powerups;
+    public int bot4ActiveCard;
+    public bool bot4Shreded = false;
+    bool bot4AtWar = false;
+
+    List<int> pot = new List<int>() { };
     private List<int> pickedCards = new() { };
-    public int currentBet = 1;
-    public int pot = 0;
+    public List<int> cardsInPlay = new() { };
 
     private bool waiting = true;
 
@@ -34,6 +49,7 @@ public class GameManagerScript : MonoBehaviour
     {
         dealCards();
         logCards();
+        reset();
         waiting = true;
     }
 
@@ -44,25 +60,24 @@ public class GameManagerScript : MonoBehaviour
         {
             aiPlays();
             logCards();
+            getWinner();
+            reset();
             waiting = true;
         }
     }
 
     public void dealCards()
     {
-        playerCards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard() };
-        bot1Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard() };
-        bot2Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard() };
-        bot3Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard() };
-        bot4Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard() };
+        playerCards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard()};
+        bot1Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard()};
+        bot2Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard()};
+        bot3Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard()};
+        bot4Cards = new List<int> { pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard(), pickCard()};
     }
 
     private void logCards()
     {
-        foreach (int x in playerCards)
-        {
-            Debug.Log(cardConversionTable[x]);
-        }
+        Debug.Log(cardConversionTable[playerActiveCard]);
     }
 
     private int pickCard()
@@ -71,7 +86,7 @@ public class GameManagerScript : MonoBehaviour
         int card = -1;
         while (!cardChecked)
         {
-            card = Random.Range(0, 53);
+            card = Random.Range(0, 38);
             if (!pickedCards.Contains(card))
             {
                 cardChecked = true;
@@ -81,52 +96,403 @@ public class GameManagerScript : MonoBehaviour
         return card;
     }
 
-    public void buyIn()
+    public void reset()
     {
-        if (playerChips - BUYINVALUE < 0)
+        if (playerCards.Count > 0)
         {
-            gameOver();
+            playerActiveCard = playerCards[Random.Range(0, playerCards.Count)];
+            playerCards.Remove(playerActiveCard);
         }
         else
         {
-            playerChips -= BUYINVALUE;
+            playerActiveCard = -1;
         }
-    }
 
-    public void call()
-    {
-        if (playerChips - (currentBet - playerBet) < 0)
+        if (bot1Cards.Count > 0)
         {
-            
+            bot1ActiveCard = bot1Cards[Random.Range(0, bot1Cards.Count)];
+            bot1Cards.Remove(bot1ActiveCard);
         }
         else
         {
-            playerChips -= currentBet - playerBet;
-            playerBet = currentBet;
+            bot1ActiveCard = -1;
         }
-    }
 
-    public void fold()
-    {
-        playerFolded = true;
-    }
-
-    public void raise(int raiseAmount)
-    {
-        if (playerChips - (currentBet + raiseAmount) - playerBet >= 0)
+        if (bot2Cards.Count > 0)
         {
-            currentBet += raiseAmount;
-            playerChips -= currentBet - playerBet;
+            bot2ActiveCard = bot2Cards[Random.Range(0, bot2Cards.Count)];
+            bot2Cards.Remove(bot2ActiveCard);
         }
         else
         {
-            Debug.Log("can't do that");
+            bot2ActiveCard = -1;
+        }
+
+        if (bot3Cards.Count > 0)
+        {
+            bot3ActiveCard = bot3Cards[Random.Range(0, bot3Cards.Count)];
+            bot3Cards.Remove(bot3ActiveCard);
+        }
+        else
+        {
+            bot3ActiveCard = -1;
+        }
+
+        if (bot4Cards.Count > 0)
+        {
+            bot4ActiveCard = bot4Cards[Random.Range(0, bot4Cards.Count)];
+            bot4Cards.Remove(bot4ActiveCard);
+        }
+        else
+        {
+            bot4ActiveCard = -1;
+        }
+
+        pot.Clear();
+    }
+
+    public void shred()
+    {
+        if (waiting)
+        {
+            playerShreded = true;
+            waiting = false;
         }
     }
+
+    public void play()
+    {
+        if (waiting)
+        {
+            playerShreded = false;
+            waiting = false;
+        }
+    }
+
 
     public void aiPlays()
     {
+        // bot 1
+        if (cardvalue(bot1ActiveCard) < Random.Range(3, 7))
+        {
+            bot1Shreded = true;
+        }
+        else
+        {
+            bot1Shreded = false;
+        }
 
+        if (cardvalue(bot2ActiveCard) < Random.Range(3, 7))
+        {
+            bot2Shreded = true;
+        }
+        else
+        {
+            bot2Shreded = false;
+        }
+
+        if (cardvalue(bot3ActiveCard) < Random.Range(3, 7))
+        {
+            bot3Shreded = true;
+        }
+        else
+        {
+            bot3Shreded = false;
+        }
+
+        if (cardvalue(bot4ActiveCard) < Random.Range(3, 7))
+        {
+            bot4Shreded = true;
+        }
+        else
+        {
+            bot4Shreded = false;
+        }
+
+    }
+
+    public void getWinner()
+    {
+        if (playerShreded)
+        {
+            playerActiveCard = -1;
+        }
+        if (bot1Shreded)
+        {
+            bot1ActiveCard = -1;
+        }
+        if (bot2Shreded)
+        {
+            bot2ActiveCard = -1;
+        }
+        if (bot3Shreded)
+        {
+            bot3ActiveCard = -1;
+        }
+        if (bot4Shreded)
+        {
+            bot4ActiveCard = -1;
+        }
+
+        List<int> total = new() { cardvalue(playerActiveCard), cardvalue(bot1ActiveCard), cardvalue(bot2ActiveCard), cardvalue(bot3ActiveCard), cardvalue(bot4ActiveCard) };
+        List<int> organized = new List<int> { total[0], total[1], total[2], total[3], total[4]};
+        organized.Sort();
+
+        if (playerActiveCard > -1)
+        {
+            pot.Add(playerActiveCard);
+        }
+        if (bot1ActiveCard > -1)
+        {
+            pot.Add(bot1ActiveCard);
+        }
+        if (bot2ActiveCard > -1)
+        {
+            pot.Add(bot2ActiveCard);
+        }
+        if (bot3ActiveCard > -1)
+        {
+            pot.Add(bot3ActiveCard);
+        }
+        if (bot4ActiveCard > -1)
+        {
+            pot.Add(bot4ActiveCard);
+        }
+
+        if (organized[4] == organized[3])
+        {
+            int warWager = 3;
+            List<int> warPlayPot = new List<int> { };
+
+            if (organized[4] == total[0])
+            {
+                if (playerCards.Count <= warWager)
+                {
+                    warWager = playerCards.Count - 1;
+                }
+
+                playerAtWar = true;
+            }
+            if (organized[4] == total[1])
+            {
+                if (bot1Cards.Count <= warWager)
+                {
+                    warWager = bot1Cards.Count - 1;
+                }
+
+                bot1AtWar = true;
+            }
+            if (organized[4] == total[2])
+            {
+                if (bot2Cards.Count <= warWager)
+                {
+                    warWager = bot2Cards.Count - 1;
+                }
+
+                bot2AtWar = true;
+            }
+            if (organized[4] == total[3])
+            {
+                if (bot3Cards.Count <= warWager)
+                {
+                    warWager = bot3Cards.Count - 1;
+                }
+
+                bot3AtWar = true;
+            }
+            if (organized[4] == total[4])
+            {
+                if (bot4Cards.Count <= warWager)
+                {
+                    warWager = bot4Cards.Count - 1;
+                }
+
+                bot4AtWar = true;
+            }
+
+            if (playerAtWar)
+            {
+                for (int i = warWager; i > 0; i--)
+                {
+                    pot.Add(pickCardFromHand(0));
+                }
+                playerActiveCard = pickCardFromHand(0);
+                warPlayPot.Add(playerActiveCard);
+            }
+            else
+            {
+                playerActiveCard = -1;
+            }
+            if (bot1AtWar)
+            {
+                for (int i = warWager; i > 0; i--)
+                {
+                    pot.Add(pickCardFromHand(1));
+                }
+                bot1ActiveCard = pickCardFromHand(1);
+                warPlayPot.Add(bot1ActiveCard);
+            }
+            else
+            {
+                bot1ActiveCard = -1;
+            }
+            if (bot2AtWar)
+            {
+                for (int i = warWager; i > 0; i--)
+                {
+                    pot.Add(pickCardFromHand(2));
+                }
+                bot2ActiveCard = pickCardFromHand(2);
+                warPlayPot.Add(bot2ActiveCard);
+            }
+            else
+            {
+                bot2ActiveCard = -1;
+            }
+            if (bot3AtWar)
+            {
+                for (int i = warWager; i > 0; i--)
+                {
+                    pot.Add(pickCardFromHand(3));
+                }
+                bot3ActiveCard = pickCardFromHand(3);
+                warPlayPot.Add(bot3ActiveCard);
+            }
+            else
+            {
+                bot3ActiveCard = -1;
+            }
+            if (bot4AtWar)
+            {
+                for (int i = warWager; i > 0; i--)
+                {
+                    pot.Add(pickCardFromHand(4));
+                }
+                bot4ActiveCard = pickCardFromHand(4);
+                warPlayPot.Add(bot4ActiveCard);
+            }
+            else
+            {
+                bot4ActiveCard = -1;
+            }
+
+            warPlayPot.Sort();
+
+            getWinner();
+        }
+        else
+        {
+            if (organized[4] == total[0])
+            {
+                Debug.Log(cardConversionTable[playerActiveCard]);
+                Debug.Log("player wins");
+                playerCards.AddRange(pot);
+            }
+            else if (organized[4] == total[1])
+            {
+                Debug.Log(cardConversionTable[bot1ActiveCard]);
+                Debug.Log("bot1 wins");
+                bot1Cards.AddRange(pot);
+            }
+            else if (organized[4] == total[2])
+            {
+                Debug.Log(cardConversionTable[bot2ActiveCard]);
+                Debug.Log("bot2 wins");
+                bot2Cards.AddRange(pot);
+            }
+            else if (organized[4] == total[3])
+            {
+                Debug.Log(cardConversionTable[bot3ActiveCard]);
+                Debug.Log("bot3 wins");
+                bot3Cards.AddRange(pot);
+            }
+            else if (organized[4] == total[4])
+            {
+                Debug.Log(bot4ActiveCard);
+                Debug.Log(cardConversionTable[bot4ActiveCard]);
+                Debug.Log("bot4 wins");
+                bot4Cards.AddRange(pot);
+            }
+        }
+    }
+
+    private void war()
+    {
+
+    }
+
+    private int pickCardFromHand(int playerID)
+    {
+        int card = -1;
+
+        if (playerID == 0)
+        {
+            if (playerCards.Count <= 0)
+            {
+                return -1;
+            }
+
+            card = playerCards[Random.Range(0, playerCards.Count)];
+            playerCards.Remove(card);
+        }
+        else if (playerID == 1)
+        {
+            if (bot1Cards.Count <= 0)
+            {
+                return -1;
+            }
+
+            card = bot1Cards[Random.Range(0, bot1Cards.Count)];
+            bot1Cards.Remove(card);
+        }
+        else if (playerID == 2)
+        {
+            if (bot2Cards.Count <= 0)
+            {
+                return -1;
+            }
+
+            card = bot2Cards[Random.Range(0, bot2Cards.Count)];
+            bot2Cards.Remove(card);
+        }
+        else if (playerID == 3)
+        {
+            if (bot3Cards.Count <= 0)
+            {
+                return -1;
+            }
+
+            card = bot3Cards[Random.Range(0, bot3Cards.Count)];
+            bot3Cards.Remove(card);
+        }
+        else if (playerID == 4)
+        {
+            if (bot4Cards.Count <= 0)
+            {
+                return -1;
+            }
+
+            card = bot4Cards[Random.Range(0, bot4Cards.Count)];
+            bot4Cards.Remove(card);
+        }
+
+        return card;
+    }
+
+    private int cardvalue(int cardID)
+    {
+        if (cardID < 0)
+        {
+            return -1;
+        }
+        else if (cardID <= 30)
+        {
+            return cardID % 10;
+        }
+        else
+        {
+            return 10;
+        }
     }
 
     public void gameOver()
