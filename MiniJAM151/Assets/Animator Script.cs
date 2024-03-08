7d229e2a13948cf9e36b6c2cf32b85b980a83a91
17thdimensionstudio <112668279+ACcreations67@users.noreply.github.com>
2024-03-08 15:40:34 -0800

    fixed some things

--- MiniJAM151/Assets/Animator Script.cs
+++ MiniJAM151/Assets/Animator Script.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class AnimatorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Prepare list
+    //referance animator
+    //add objective function
+        //
 }
